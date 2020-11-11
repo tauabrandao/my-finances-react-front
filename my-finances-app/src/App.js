55100f54fafa167bbae026a4a2f5721a1c8303a6
Tauã Brandão <brandao.taua@gmail.com>
2020-11-10 22:33:17 -0300

    Bootswatch added

--- my-finances-app/src/App.js
+++ my-finances-app/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    nome: 'Tauã',
+    nome: null,
     numero1: null,
     numero2: null,
     resultado: null
   }
-  
+
   somar = () => {
     const result = parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    this.setState({resultado: result})
+    this.setState({ resultado: result })
   }
 
   render() {
@@ -27,16 +27,16 @@ class App extends React.Component {
         <div>
           <label>Numero 1:</label>
           <input type="text" value={this.state.numero1}
-          onChange={(e) => this.setState({numero1: e.target.value}) }/>
-          <br/>
+            onChange={(e) => this.setState({ numero1: e.target.value })} />
+          <br />
 
           <label>Numero 2:</label>
           <input type="text" value={this.state.numero2}
-          onChange={(e) => this.setState({numero2: e.target.value}) }/>
-          <br/>
+            onChange={(e) => this.setState({ numero2: e.target.value })} />
+          <br />
 
           <button onClick={this.somar}>Somar</button>
-          <br/>
+          <br />
           A soma dos números é {this.state.resultado}
         </div>
 
