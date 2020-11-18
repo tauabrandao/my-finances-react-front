daad50a006b2151fca4e56f8be0a153ee9ff3d76
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-11-17 21:38:49 -0300

    saving and retrieving logged-in user through local storage

--- my-finances-app/src/views/home.js
+++ my-finances-app/src/views/home.js
@@ -5,14 +5,22 @@ import axios from 'axios'
 class Home extends React.Component {
 
     state = {
-        saldo : 0
+        saldo : 0,
+        nomeUsuario: null
     }
 
     componentDidMount(){
-        let url = 'http://localhost:8080/api/usuarios/1/saldo'
+        const usuarioLogadoFromLocalStorage = localStorage.getItem('_usuario_logado')
+        const usuarioLogado = JSON.parse(usuarioLogadoFromLocalStorage)
+
+        const url = `http://localhost:8080/api/usuarios/${usuarioLogado.id}/saldo`
+
+        console.log(usuarioLogado)
+        console.log(url)
         axios.get(url)
         .then(response => {
             this.setState({saldo: response.data})
+            this.setState({nomeUsuario: usuarioLogado.nome})
         }).catch(erro => {
             console.error(erro.response)
         })
@@ -21,7 +29,7 @@ class Home extends React.Component {
     render(){
         return(
             <div className="jumbotron">
-                <h1 className="display-3">Bem vindo!</h1>
+                <h1 className="display-3">Bem vindo {this.state.nomeUsuario}!</h1>
                 <p className="lead">Esse é seu sistema de finanças.</p>
         <p className="lead">Seu saldo para o mês atual é de R$ {this.state.saldo}</p>
                 <hr className="my-4"/>
