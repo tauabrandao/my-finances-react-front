b6c5e032e38e7115acd6e5be1badd00314c73d38
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-19 16:31:24 -0300

    Adding state on Lancamento register form, and handleChange function

--- my-finances-app/src/views/lancamentos/cadastro-lancamentos.js
+++ my-finances-app/src/views/lancamentos/cadastro-lancamentos.js
@@ -9,9 +9,30 @@ import LancamentoService from '../../app/service/lancamentoService'
 
 class CadastroLancamentos extends React.Component {
 
+    state = {
+        id: null,
+        descricao: '',
+        valor: '',
+        mes: '',
+        ano: '',
+        tipo: '',
+        status: ''
+    }
+
     constructor() {
         super();
         this.service = new LancamentoService();
+    }
+
+    submit = () =>{
+        console.log(this.state)
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({ [name] : value})
     }
 
     render() {
@@ -24,57 +45,77 @@ class CadastroLancamentos extends React.Component {
                 <div className="row">
                     <div className="col-md-12">
                         <FormGroup id="inputDescricao" label="Descrição: *">
-                            <input id="inputDescricao" type="text" className="form-control" />
+                            <input id="inputDescricao"
+                                name="descricao" 
+                                type="text" 
+                                value={this.state.descricao}
+                                onChange={this.handleChange}
+                                className="form-control" />
                         </FormGroup>
                     </div>
                 </div>
                 <div className="row">
-                    <div class="col-md-6">
+                    <div className="col-md-6">
                         <FormGroup id="inputAno" label="Ano: *">
                             <input id="inputAno"
                                 type="text"
                                 name="ano"
+                                value={this.state.ano}
+                                onChange={this.handleChange}
                                 className="form-control" />
                         </FormGroup>
 
 
                     </div>
-                    <div class="col-md-6">
+                    <div className="col-md-6">
                         <FormGroup id="inputMes" label="Mês: *">
                             <SelectMenu id="inputMes"
                                 name="mes"
+                                value={this.state.mes}
+                                onChange={this.handleChange}
                                 lista={meses}
                                 className="form-control" />
                         </FormGroup>
                     </div>
                 </div>
                 <div className="row">
-                    <div class="col-md-4">
+                    <div className="col-md-4">
                         <FormGroup id="inputValor" label="Valor: *">
                             <input id="inputValor"
                                 type="text"
+                                value={this.state.valor}
+                                name="valor"
+                                onChange={this.handleChange}
                                 className="form-control" />
                         </FormGroup>
 
 
                     </div>
-                    <div class="col-md-4">
+                    <div className="col-md-4">
                         <FormGroup id="inputTipo" label="Tipo: *">
-                            <SelectMenu id="inputTipo" lista={tipos}
+                            <SelectMenu id="inputTipo"
+                                lista={tipos}
+                                name="tipo"
+                                value={this.state.tipo}
+                                onChange={this.handleChange}
                                 className="form-control" />
                         </FormGroup>
                     </div>
-                    <div class="col-md-4">
+                    <div className="col-md-4">
 
                         <FormGroup id="inputStatus" label="Status: ">
-                            <input type="text" className="form-control" disabled />
+                            <input type="text"
+                                name="status"
+                                value={this.state.status}
+                                className="form-control"
+                                disabled />
                         </FormGroup>
 
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
-                        <button className="btn btn-success">Salvar</button>
+                        <button onClick={this.submit} className="btn btn-success">Salvar</button>
                         <button className="btn btn-danger">Cancelar</button>
                     </div>
 
