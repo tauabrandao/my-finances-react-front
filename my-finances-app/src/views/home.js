2b5c52f9f5ef2d8b60346796f11f17c09d30a0e6
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-24 18:46:09 -0300

    Adjustments to show balance in home page

--- my-finances-app/src/views/home.js
+++ my-finances-app/src/views/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import UsuarioService from '../app/service/usuarioService'
 import LocalStorageService from '../app/service/localStorageService'
+import currencyFormatter from 'currency-formatter'
 
 class Home extends React.Component {
 
@@ -32,12 +33,12 @@ class Home extends React.Component {
             <div className="jumbotron">
                 <h1 className="display-3">Bem vindo {this.state.nomeUsuario}!</h1>
                 <p className="lead">Esse é seu sistema de finanças.</p>
-        <p className="lead">Seu saldo para o mês atual é de R$ {this.state.saldo}</p>
+        <p className="lead">Seu saldo para o mês atual é de R$ {currencyFormatter.format(this.state.saldo, { locale: 'pt-BR' })}</p>
                 <hr className="my-4"/>
                 <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
                 <p className="lead">
-                <a className="btn btn-primary btn-lg" href="#/cadastro-usuarios" role="button"><i className="fa fa-users"></i><i className="pi pi-plus"/>  Cadastrar Usuário</a>
-                <a className="btn btn-danger btn-lg" href="#/cadastro-lancamentos" role="button"><i className="fa fa-users"></i><i className="pi pi-plus"/>  Cadastrar Lançamento</a>
+                <a className="btn btn-primary btn-lg" href="#/cadastro-usuarios" role="button"><i className="fa fa-users"></i><i className="pi pi-users"/>  Cadastrar Usuário</a>
+                <a className="btn btn-danger btn-lg" href="#/cadastro-lancamentos" role="button"><i className="fa fa-users"></i><i className="pi pi-money-bill"/>  Cadastrar Lançamento</a>
                 </p>
             </div>
         )
